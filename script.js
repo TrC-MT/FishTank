0b85ef3eb751663635db0a527363959dc3d45f31
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-10-25 08:28:54 -0600

    Added some User Interface

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ Developers note: This is a personal project. I'm making it for fun. I got the id
 //==========================================================================================
 window.setTimeout( //After a certain amount of time, in this case 10 milliseconds
     function(){
-        ChangeIntervals(); //Run the ChangeIntervals function
-    }, 10
+        GetIntervals(); //Run the GetIntervals function
+    }, 1000
 );
 
+function GetIntervals(){
+    const MaxInterval = Number(document.getElementById('MaxIntervalInput').value) //Go to the html, find the element with the id of MaxIntervalInput, grab the value of that, make it a number, set it to MaxInterval
+    const MinInterval = Number(document.getElementById('MinIntervalInput').value)
+    ChangeIntervals(MaxInterval, MinInterval)
+}
+
 var interval = 3000 //It's only set to be a global variable. The number that it's set to shouldn't matter, it's just there for debugging purposes
-const MaxInterval = 30000 //Still figuring out the timings, which is why they are long. Eventually they should be smaller ex: between 8 and 4
-const MinInterval = 20000
 
-
-function ChangeIntervals(){
+function ChangeIntervals(MaxInterval, MinInterval){
     var DecInterval = Math.random() * (MaxInterval - MinInterval) + MinInterval; //Randomly pick a number between the max and min
     interval = Math.floor(DecInterval); //Set the interval to that number, removing the decimals for proper timing.
 
@@ -27,7 +30,7 @@ function ChangeIntervals(){
     ChangeFishWidth();
     ChangeFishSpeed(); //It might become independant and run on it's own timing.
 
-    setTimeout(ChangeIntervals, interval) //Re-run this function after the interval
+    setTimeout(GetIntervals, interval) //Re-run GetIntervals after the interval
 }
 
 //-------------------------------------------------------------
@@ -36,7 +39,7 @@ function getRandomColorNumber() {
     return Math.random() * 255; //255 is the max rgb amount
 }
 function ChangeFishColor(){
-    for(let i = 1; i < document.getElementsByTagName('span').length +1; i++){ //The amount of divs, spans, and fish are the same. I just used what it's selecting for readability
+    for(let i = 1; i < document.getElementsByTagName('span').length +1; i++){ //The amount of spans and fish are the same. I just used what it's selecting for readability
         const red = getRandomColorNumber();
         const green = getRandomColorNumber();
         const blue = getRandomColorNumber();
@@ -56,8 +59,11 @@ function ChangeFishWidth(){ //Change the width/size of the iterated fish to a ra
 //-------------------------
 
 function ChangeFishSpeed(){
-    for(let i = 1; i < document.getElementsByTagName('div').length +1; i++){ // i goes from 1 through 4 //Iterates only once?
-        var Duration = Math.random() * (8 - 4) + 4 // Hopefully these can be smaller too. They are just larger for debugging purposes.
+    const max_duration = Number(document.getElementById('MaxDurationInput').value)
+    const min_duration = Number(document.getElementById('MinDurationInput').value)
+    for(let i = 1; i < document.getElementsByTagName('img').length +1; i++){ // i goes from 1 through 4 //Iterates only once?
+        var dec_Duration = Math.random() * (max_duration - min_duration) + min_duration // Hopefully these can be smaller too. They are just larger for debugging purposes.
+        var Duration = Math.floor(dec_Duration)
         document.getElementById(`${i}b`).style.animationDuration = Duration + 's' //This is how long it takes for the fish to get from one side of the screen to another. It is NOT how long it takes to go from one side and back.
 
         //Yes I made them individual so that eventually I can make them start in different locations. I agree it would be easier code-wise to put them together.
@@ -91,12 +97,11 @@ function ChangeFishADirection(){
     a += 1 //Keeps track of which direction it's going.
     if(a%2 == 0){
         console.log('a is even')
-        document.getElementById(`1f`).style.transform = 'rotateY(180deg)'
-        // .setAttribute('style', 'transform:rotateY(180deg)')
+        document.getElementById(`1f`).style.transform = 'rotateY(0deg)'
     }
     else{
         console.log('a is odd')
-        document.getElementById(`1f`).style.transform = 'rotateY(-180deg)' //Why does this flip back to 180?
+        document.getElementById(`1f`).style.transform = 'rotateY(-180deg)'
     }
 }
 var b = 0
@@ -104,7 +109,7 @@ function ChangeFishBDirection(){
     console.log('Changing B direction')
     b += 1
     if(b%2 == 0){
-        document.getElementById(`2f`).style.transform = 'rotateY(180deg)'
+        document.getElementById(`2f`).style.transform = 'rotateY(0deg)'
     }
     else{
         document.getElementById(`2f`).style.transform = 'rotateY(-180deg)'
@@ -115,7 +120,7 @@ function ChangeFishCDirection(){
     console.log('Changing C direction')
     c += 1
     if(c%2 == 0){
-        document.getElementById(`3f`).style.transform = 'rotateY(180deg)'
+        document.getElementById(`3f`).style.transform = 'rotateY(0deg)'
     }
     else{
         document.getElementById(`3f`).style.transform = 'rotateY(-180deg)'
@@ -126,7 +131,7 @@ function ChangeFishDDirection(){
     console.log('Changing D direction')
     d += 1
     if(d%2 == 0){
-        document.getElementById(`4f`).style.transform = 'rotateY(180deg)'
+        document.getElementById(`4f`).style.transform = 'rotateY(0deg)'
     }
     else{
         document.getElementById(`4f`).style.transform = 'rotateY(-180deg)'
